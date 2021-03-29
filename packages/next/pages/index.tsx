8d8e59605b6cf63278d2a9bdce19304a4980d1ed
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-03-29 22:55:49 +0200

    feat: global styles & index page

--- packages/next/pages/index.tsx
+++ packages/next/pages/index.tsx
@@ -1,26 +1,50 @@
 import Head from "next/head";
-import Link from "next/link";
 import Login from "../components/Login";
 import usePosts from "../hooks/usePosts";
-import styles from "../styles/Home.module.css";
-import {getFirst100Posts} from "../utils/graphql"
+import styles from "../styles/Home.module.scss";
+import Post from "../components/PostListItem";
+import { useRouter } from "next/router";
 
 const Home: React.FC = () => {
-  const posts = usePosts()
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const router = useRouter();
+	const posts = usePosts();
+	console.log(posts);
 
-      <div>
-        <Login />
-      </div>
-
-      {posts.map(post => <Link href={`/post/${post.id}`}><a><div>{post.content}</div></a></Link>)}
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div>
+				<Login />
+			</div>
+			<div className={styles.container}>
+				<div className={styles.containerHead}>
+					<button
+						type="button"
+						className={styles.btn}
+						onClick={() => router.push("/add-post")}
+					>
+						+ Add New
+					</button>
+				</div>
+				<ul>
+					{posts.map((post) => (
+						<Post
+							key={post.id}
+							author={post.author}
+							content={post.content}
+							id={post.id}
+							replyTo={post.replyTo}
+							likes={post.likes}
+							dislikes={post.dislikes}
+						/>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
