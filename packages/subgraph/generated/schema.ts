9c27774688522d4ff9678bacc92bfce2fd38b69e
Entreprenerd <valeranijr@gmail.com>
2021-03-29 15:56:43 +0200

    feat: mumbai subgraph

--- packages/subgraph/generated/schema.ts
+++ packages/subgraph/generated/schema.ts
@@ -12,7 +12,7 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class Gravatar extends Entity {
+export class Post extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
@@ -20,17 +20,17 @@ export class Gravatar extends Entity {
 
   save(): void {
     let id = this.get("id");
-    assert(id !== null, "Cannot save Gravatar entity without an ID");
+    assert(id !== null, "Cannot save Post entity without an ID");
     assert(
       id.kind == ValueKind.STRING,
-      "Cannot save Gravatar entity with non-string ID. " +
+      "Cannot save Post entity with non-string ID. " +
         'Considering using .toHex() to convert the "id" to a string.'
     );
-    store.set("Gravatar", id.toString(), this);
+    store.set("Post", id.toString(), this);
   }
 
-  static load(id: string): Gravatar | null {
-    return store.get("Gravatar", id) as Gravatar | null;
+  static load(id: string): Post | null {
+    return store.get("Post", id) as Post | null;
   }
 
   get id(): string {
@@ -42,30 +42,155 @@ export class Gravatar extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get owner(): Bytes {
-    let value = this.get("owner");
+  get publishedAtBlock(): BigInt {
+    let value = this.get("publishedAtBlock");
+    return value.toBigInt();
+  }
+
+  set publishedAtBlock(value: BigInt) {
+    this.set("publishedAtBlock", Value.fromBigInt(value));
+  }
+
+  get author(): Bytes {
+    let value = this.get("author");
     return value.toBytes();
   }
 
-  set owner(value: Bytes) {
-    this.set("owner", Value.fromBytes(value));
+  set author(value: Bytes) {
+    this.set("author", Value.fromBytes(value));
   }
 
-  get displayName(): string {
-    let value = this.get("displayName");
+  get content(): string {
+    let value = this.get("content");
     return value.toString();
   }
 
-  set displayName(value: string) {
-    this.set("displayName", Value.fromString(value));
+  set content(value: string) {
+    this.set("content", Value.fromString(value));
   }
 
-  get imageUrl(): string {
-    let value = this.get("imageUrl");
+  get replyTo(): string {
+    let value = this.get("replyTo");
     return value.toString();
   }
 
-  set imageUrl(value: string) {
-    this.set("imageUrl", Value.fromString(value));
+  set replyTo(value: string) {
+    this.set("replyTo", Value.fromString(value));
+  }
+
+  get likes(): i32 {
+    let value = this.get("likes");
+    return value.toI32();
+  }
+
+  set likes(value: i32) {
+    this.set("likes", Value.fromI32(value));
+  }
+
+  get dislikes(): i32 {
+    let value = this.get("dislikes");
+    return value.toI32();
+  }
+
+  set dislikes(value: i32) {
+    this.set("dislikes", Value.fromI32(value));
+  }
+}
+
+export class Like extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Like entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Like entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Like", id.toString(), this);
+  }
+
+  static load(id: string): Like | null {
+    return store.get("Like", id) as Like | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get author(): Bytes {
+    let value = this.get("author");
+    return value.toBytes();
+  }
+
+  set author(value: Bytes) {
+    this.set("author", Value.fromBytes(value));
+  }
+
+  get post(): string {
+    let value = this.get("post");
+    return value.toString();
+  }
+
+  set post(value: string) {
+    this.set("post", Value.fromString(value));
+  }
+}
+
+export class Dislike extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Dislike entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Dislike entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Dislike", id.toString(), this);
+  }
+
+  static load(id: string): Dislike | null {
+    return store.get("Dislike", id) as Dislike | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get author(): Bytes {
+    let value = this.get("author");
+    return value.toBytes();
+  }
+
+  set author(value: Bytes) {
+    this.set("author", Value.fromBytes(value));
+  }
+
+  get post(): string {
+    let value = this.get("post");
+    return value.toString();
+  }
+
+  set post(value: string) {
+    this.set("post", Value.fromString(value));
   }
 }
--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,22 +1,49 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { Address } from '@graphprotocol/graph-ts'
+import { Like as LikeEvent, Dislike as DislikeEvent, Post as PostEvent } from '../generated/Microblog/MicroBlog'
+import { Post, Like, Dislike } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+const fromEventToId = (from: Address, postId: string): string => from.toHex() + '-' + postId
+
+export function handleNewPost(event: PostEvent): void {
+  let id = event.transaction.hash.toHex()
+  let post = new Post(id)
+  post.likes = 0
+  post.dislikes = 0
+  post.author = event.transaction.from
+  post.content = event.params.content
+  post.replyTo = event.params.replyTo
+  post.publishedAtBlock = event.block.number
+  post.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleLike(event: LikeEvent): void {
+  let post = Post.load(event.params.postId)
+
+  let likeId = fromEventToId(event.transaction.from, event.params.postId)
+  let previous = Like.load(likeId)
+  if(!previous){
+    post.likes += 1
+    post.save()
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+  let like = new Like(likeId)
+  like.author = event.transaction.from
+  like.post = event.params.postId
+  like.save() 
+}
+
+export function handleDislike(event: DislikeEvent): void {
+  let post = Post.load(event.params.postId)
+
+  let dislikeId = fromEventToId(event.transaction.from, event.params.postId)
+  let previous = Dislike.load(dislikeId)
+  if(!previous){
+    post.dislikes += 1
+    post.save()
+  }
+
+  let dislike = new Dislike(dislikeId)
+  dislike.author = event.transaction.from
+  dislike.post = event.params.postId
+  dislike.save() 
 }
