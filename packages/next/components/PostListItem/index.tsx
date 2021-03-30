1b9bdf82e7b52fb7239f9bf3811b4827322b07cc
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-03-30 13:09:02 +0200

    fix: it now updates rating counter when rating

--- packages/next/components/PostListItem/index.tsx
+++ packages/next/components/PostListItem/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Post } from "../../types/post";
 import { ethers } from "ethers";
@@ -14,12 +14,29 @@ const PostListItem: React.FC<Post> = ({
 	id,
 	likes,
 }) => {
-	const handleRating = (type: "like" | "dislike") => {
+	const [dislikesCount, setDislikesCount] = useState(0);
+	const [likesCount, setLikesCount] = useState(0);
+
+	useEffect(() => {
+		setLikesCount(likes);
+		setDislikesCount(dislikes);
+	}, [likes, dislikes]);
+
+	const handleRating = async (type: "like" | "dislike") => {
 		const provider = new ethers.providers.Web3Provider(
 			(window as any).ethereum
 		);
 		const contract = getContract(provider.getSigner());
-		contract[type](id);
+
+		if (type === "like") {
+			await contract.like(id);
+			setLikesCount((oldLikes) => oldLikes + 1);
+		}
+
+		if (type === "dislike") {
+			await contract.dislike(id);
+			setDislikesCount((oldLikes) => oldLikes + 1);
+		}
 	};
 
 	return (
@@ -49,10 +66,10 @@ const PostListItem: React.FC<Post> = ({
 								<path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
 							</g>
 						</svg>
-						<p>{likes}</p>
+						<p>{likesCount}</p>
 					</button>
 					<button onClick={() => handleRating("dislike")}>
-						<p>{dislikes}</p>
+						<p>{dislikesCount}</p>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							width="15"
