d93858ed6209156bf68e4f4613807eb602e18b13
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-03-30 13:08:06 +0200

    feat: replies component

---
+++ packages/next/components/Replies/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import useReplies from "../../hooks/useReplies";
+import { Post } from "../../types";
+import ReplyItem from "../ReplyItem";
+
+interface RepliesProps {
+	id: string;
+}
+
+const Replies: React.FC<RepliesProps> = ({ id }) => {
+	const replies = useReplies(id);
+
+	return (
+		<ul>
+			{replies.map((reply: Post) => (
+				<ReplyItem
+					author={reply.author}
+					content={reply.content}
+					dislikes={reply.dislikes}
+					likes={reply.likes}
+					id={reply.id}
+					publishedAtBlock={reply.publishedAtBlock}
+					replyTo={reply.replyTo}
+					key={reply.id}
+				/>
+			))}
+		</ul>
+	);
+};
+
+export default Replies;
